489a58704fd719d0a95e992c24c41cb1e5c1b25c
Alex <adr9702297@gmail.com>
2023-02-12 00:18:53 -0600

    added functionality that allows the user to upload images to firestore db

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,3 +23,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,16 +1,28 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { MdAddPhotoAlternate } from "react-icons/md";
-import { auth } from "../../firebase";
+import { auth, storage, db } from "../../firebase";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  getStorage,
+} from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const RegisterPage = () => {
-  const [username, setUsername] = useState("");
+  const [displayName, setDisplayName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [file, setFile] = useState("");
   const [error, setError] = useState(false);
+  console.log(file);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,19 +34,59 @@ const RegisterPage = () => {
         auth,
         email,
         password
-      )
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
+      );
+      const storage = getStorage();
 
-          console.log(user);
-          // ...
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // ..
-        });
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          setError(true);
+          console.log(error);
+          // Handle unsuccessful uploads
+        },
+        () => {
+          //   Handle successful uploads on complete
+          //   For instance, get the download URL: https://firebasestorage.googleapis.com/...
+
+          //
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            console.log("File available at", downloadURL);
+            await updateProfile(response.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", response.user.uid), {
+              uid: response.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
     } catch (error) {
       console.log(error);
       setError(true);
@@ -65,8 +117,8 @@ const RegisterPage = () => {
           <input
             type="text"
             id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
             className="border border-gray-400 p-2 w-full bg-zinc-400"
           />
         </div>
